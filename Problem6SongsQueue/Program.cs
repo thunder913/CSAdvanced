f0d72d8f154f7f587931ad6810f6539734394ba0
thunder913 <63390709+thunder913@users.noreply.github.com>
2020-05-23 21:05:20 +0300

    Add files via upload

---
+++ Problem6SongsQueue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Problem6SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstSongs = Console.ReadLine().Split(", ");
+            var queue = new Queue<string>(firstSongs);
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split();
+                if (input[0] == "Add")
+                {
+                    string songName = input[1];
+                    for (int i = 2; i < input.Length; i++)
+                    {
+                        songName += " " + input[i];
+                    }
+                    if (!queue.Contains(songName))
+                    {
+                        queue.Enqueue(songName);
+                    }
+                    else 
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }
+                }
+                else if (input[0] == "Play")
+                {
+                    if (queue.Any())
+                    {
+                        queue.Dequeue();
+                    }
+                }
+                else if (input[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", queue)); 
+                }
+
+                if (!queue.Any())
+                {
+                    Console.WriteLine($"No more songs!");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Problem7TruckTour/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Problem7TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPumps = int.Parse(Console.ReadLine());
+            var queueDistance = new Queue<int>();
+            var queueFuel = new Queue<int>();
+            for (int i = 0; i < totalPumps; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                queueFuel.Enqueue(input[0]);
+                queueDistance.Enqueue(input[1]);
+            }
+            var fuelSaved = new List<int>(queueFuel);
+            var distanceSaved = new List<int>(queueDistance);
+            for (int i = 0; i < totalPumps; i++)
+            {
+                int currentFuel = 0;
+                for (int j = 0; j < totalPumps; j++)
+                {
+                    int fuel = queueFuel.Dequeue();
+                    int distance = queueDistance.Dequeue();
+                    if (currentFuel + fuel < distance)
+                    {
+                        break;
+                    }
+                    currentFuel += fuel - distance;
+                    if (j == totalPumps-1)
+                    {
+                        Console.WriteLine(i);
+                        return;
+                    }
+                }
+                queueFuel = new Queue<int>(fuelSaved);
+                queueDistance = new Queue<int>(distanceSaved);
+                for (int k = 0; k <= i; k++)
+                {
+                    int currentF = queueFuel.Dequeue();
+                    int currentD = queueDistance.Dequeue();
+                    queueFuel.Enqueue(currentF);
+                    queueDistance.Enqueue(currentD);
+                }
+                
+            }
+        }
+    }
+}
