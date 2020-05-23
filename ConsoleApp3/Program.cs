6e11237982e88538b393233bbb0b3614e291b28d
thunder913 <63390709+thunder913@users.noreply.github.com>
2020-05-23 20:58:57 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+            var input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int toPush = input[0];
+            int toPop = input[1];
+            int num = input[2];
+
+            for (int i = 0; i < toPush; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+
+            for (int i = 0; i < toPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(num))
+            {
+                Console.WriteLine("true");
+            }
+            else if(!stack.Any())
+            {
+                Console.WriteLine(0);
+            }
+            else 
+            {
+                Console.WriteLine(stack.Min());
+
+            }
+        }
+    }
+}
---
+++ MaxAndMinElements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MaxAndMinElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+            int commands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < commands; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var firstNNum = input[0];
+                if (firstNNum == 1)
+                {
+                    stack.Push(input[1]);
+                }
+                else if (firstNNum == 2)
+                {
+                    stack.Pop();
+                }
+                else if (firstNNum == 3)
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                    
+                }
+                else if (firstNNum == 4)
+                {
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
